45df5210405d056bffe372fd7d4d02771f8bcc21
Alex Waller <alex@pmlhomeloans.com>
2022-04-04 08:51:34 -0400

    update

---
+++ Test Fantasy RPG/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Animator animator;
+    public float speed = 7.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Horizontal Movement
+        Vector3 horizontalInput = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        transform.position += horizontalInput * Time.deltaTime * speed;
+
+        //Vertical Movement
+        Vector3 verticalInput = new Vector3(0f, Input.GetAxis("Vertical"), 0f);
+        transform.position += verticalInput * Time.deltaTime * speed;
+
+        animator.SetFloat("Speed", horizontalInput.magnitude + verticalInput.magnitude);
+    }
+}
